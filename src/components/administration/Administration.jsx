93e4a3ab40828373bae600c0d5443da810b22abc
Edu4rdoNeves <dudunekss@gmail.com>
2024-10-23 14:53:30 -0300

    feat: finsh menus

--- src/components/administration/Administration.jsx
+++ src/components/administration/Administration.jsx
@@ -1,10 +1,21 @@
 import './Administration.css'; 
+import { useNavigate } from 'react-router-dom'; // Importando useNavigate do react-router-dom
+
 
 const Administration = () => {
+  const navigate = useNavigate(); // Hook para acessar o objeto de navegação
+
+  const handleBack = () => {
+    navigate(-1); // Volta para a tela anterior
+  };
+
   return (
     <div className={`menu-page`}>
       <div className="sidebar-left"></div> {/* Barra esquerda */}
       <div className="main-content">
+         <button className="back-button" onClick={handleBack}>
+          <i className="fas fa-arrow-left"></i> Voltar
+        </button>
         <div className="logo-container"> {/* Container para a logo e o título */}
         <img src="/assets/task.svg" className="logo" alt="task animation" />
         <h1 style={{ color: '#1e90ff' }}>Menu de Administração!</h1>
